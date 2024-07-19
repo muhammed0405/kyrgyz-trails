a069967d9ecc57b52d5bb2424056c778bb77f4c3
muhammet2005 <maxmudgamer009@gmail.com>
2024-07-19 09:08:25 +0700

    added redux toolkit and slices

--- app/redux/store.tsx
+++ app/redux/store.tsx
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import authReducer from "./features/authSlice"
+import { useAppSelector } from "./hooks"
+import { apiSlice } from "./services/appSlice"
 export const makeStore = () =>
 	configureStore({
-		reducer: {},
+		reducer: {
+			auth: authReducer
+		},
 		devTools: process.env.NODE_ENV !== 'production',
 	})
 
 export type AppStore = ReturnType<typeof makeStore>
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
+
+
