00d4c7ddb1ea19de1975b1c62dac2ebde06eb6de
muhammet2005 <maxmudgamer009@gmail.com>
2024-07-26 12:30:28 +0700

    added register logic

--- app/pages/region-details/[id]/page.tsx
+++ app/pages/region-details/[id]/page.tsx
@@ -5,12 +5,9 @@ import Image from "next/image"
 import styles from "../../../styles/regions_details.module.scss"
 import { useTypedSelectorHook } from "@/app/Redux/customHooks/useTypedSelectorHook"
 import { notFound } from "next/navigation"
-<<<<<<< HEAD:app/pages/regions/[id]/page.tsx
 import { useEffect } from "react"
 import { UseTypedDispatch } from "@/app/Redux/customHooks/useTypedDispatch"
-=======
 import { ITour } from "@/app/Redux/Interfaces/tourReducerType"
->>>>>>> 8c514533d95d675d93ed1bdec93026cb7c2949be:app/pages/region-details/[id]/page.tsx
 
 export default function RegionPage({ params }: { params: { id: string } }) {
 	const tour = useTypedSelectorHook(state => state.tours.tour)
@@ -30,7 +27,6 @@ export default function RegionPage({ params }: { params: { id: string } }) {
 	console.log("tour", tour)
 	return (
 		<div className={styles.container}>
-		
 			<Image
 				className={styles.image}
 				width={200}
@@ -38,7 +34,7 @@ export default function RegionPage({ params }: { params: { id: string } }) {
 				src={region.imageUrl}
 				alt={region.name}
 			/>
-				<h1 className={styles.title}>{region.name}</h1>
+			<h1 className={styles.title}>{region.name}</h1>
 			<p className={styles.shortDescription}>{region.shortDescription}</p>
 			<div className={styles["rating-reviews"]}>
 				<span className={styles.rating}>Rating: {region.rating}</span>
@@ -47,24 +43,24 @@ export default function RegionPage({ params }: { params: { id: string } }) {
 			<h2 className={styles["section-title"]}>About</h2>
 			<p>{region.longDescription}</p>
 			<h2 className={styles["section-title"]}>Tours</h2>
-		<div className={styles.tourCardsWrapper}>
-		{region.tours.map((tour:ITour )=> (
-				<div key={tour.id} className={styles.tourCard}>
-					<Image
-						className={styles["tour-image"]}
-						width={200}
-						height={150}
-						src={tour.imageUrl}
-						alt={tour.name}
-					/>
-					<h3 className={styles["tour-title"]}>{tour.name}</h3>
-					
-					<p className={styles["tour-description"]}>{tour.description}</p>
-					<p className={styles["tour-duration"]}>Duration: {tour.duration}</p>
-					<p className={styles["tour-price"]}>Price: ${tour.price}</p>
-				</div>
-			))}
-		</div>
+			<div className={styles.tourCardsWrapper}>
+				{region.tours.map((tour: ITour) => (
+					<div key={tour.id} className={styles.tourCard}>
+						<Image
+							className={styles["tour-image"]}
+							width={200}
+							height={150}
+							src={tour.imageUrl}
+							alt={tour.name}
+						/>
+						<h3 className={styles["tour-title"]}>{tour.name}</h3>
+
+						<p className={styles["tour-description"]}>{tour.description}</p>
+						<p className={styles["tour-duration"]}>Duration: {tour.duration}</p>
+						<p className={styles["tour-price"]}>Price: ${tour.price}</p>
+					</div>
+				))}
+			</div>
 		</div>
 	)
 }
